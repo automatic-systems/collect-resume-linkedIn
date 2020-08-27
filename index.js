221da905e3ab4d6186e6bede5d67e7a150d7cd3e
Nikhilesh Yadav <nikhileshofficial@gmail.com>
2020-08-27 08:01:34 +0530

    +clean

--- index.js
+++ index.js
@@ -1,21 +1,14 @@
 require('dotenv').config()
-var username = process.env.username
-var password = process.env.password
-var executable = process.env.chrome_executable
-var outdir = process.env.out_dir
-var parallel = process.env.parallel
-var limit = process.env.limit
 let error = require('debug')('error')
 var loaded = require('debug')('loaded')
 var { argv } = require('yargs')
-    // .usage('Usage: $0 -username [ linkedin email ]\n -password [ linkedin password ]\n -filter [regular expression of profile names (optional)]\n -executable [chrome executable]\n -outdir [where to output user Profiles] -ntab [Number of tab instances to use]')
     .demandOption(['username', 'password', 'executable-path', 'user-data-dir'])
     .example('$0 -u nikhilesh@email.com -p <password> -f chauhan -e /bin/chrome -o ./profiles -n 10 -l 55')
     .example('$0 -u n@t -p <p> -e /bin/chrome -ud "~/.config/google/chrome/User Data" -pd "Profile 2"')
     .default({
         'filter': '.',
-        'outdir': process.env.OUTDIR || './',
-        'ntab': process.env.PARALLEL || 10,
+        'outdir': process.env.OUTDIR || './profiles',
+        'ntab': process.env.PARALLEL || 1,
         'limit': Infinity,
         'profile-dir': 'Default',
         headless: false
@@ -41,7 +34,7 @@ require('./init').init({ executablePath, userDataDir, profileDir, headless }).th
     var count = await downloadProfiles({ outDir: outdir, parallel, limit, newPage, page, filter })
     loaded(`SuccessFully created :${count} Profile pdfs`)
 })
-//DEBUG=loaded,downloaded,error node index.js -u nikhileshofficial@gmail.com -p linkedIn@123 -e "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" --ud "C:\Users\Shailesh\AppData\Local\Google\Chrome\User Data" --pd "Default" -l 10 -n 1 -o ./profiles
+//DEBUG=loaded,downloaded,error node index.js -u nikhileshofficial@gmail.com -p --pass-- -e "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" --ud "C:\Users\Shailesh\AppData\Local\Google\Chrome\User Data" --pd "Default" -l 10 -n 1 -o ./profiles
 process.on('unhandledRejection', e => {
     error(`Unhandled Error ${e.message}`)
 });
